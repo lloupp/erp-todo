a1511697cae093e71b45151bcd0bc898289d543e
Eduardo <86481812+lloupp@users.noreply.github.com>
2026-09-12 16:01:19 -0300

    refactor: share hardened WSGI startup with Waitress

--- run_prod.py
+++ run_prod.py
@@ -5,17 +5,7 @@ from waitress import serve
 
 load_dotenv()
 
-import app as app_module
-from db_migrations import ensure_database
-from password_security import hash_password, verify_password
-from production_guards import install_production_guards
-
-# Troca os helpers legados do modulo principal por KDF adaptativa mantendo
-# compatibilidade de leitura com hashes antigos. As rotas olham os globals do
-# modulo em runtime, entao novas senhas e validacoes passam por estes helpers.
-app_module.hash_password = hash_password
-app_module.verify_password = verify_password
-app = app_module.app
+from wsgi import app
 
 
 if __name__ == '__main__':
@@ -26,15 +16,6 @@ if __name__ == '__main__':
             '  Windows: set SECRET_KEY=sua-chave-aqui\n'
             '  Linux:   export SECRET_KEY=sua-chave-aqui'
         )
-
-    ensure_database(
-        app.config['DATABASE'],
-        hash_password=hash_password,
-        message_seed=app_module.MENSAGENS_MODELO_SEED,
-        pipeline_etapas=app_module.PIPELINE_ETAPAS,
-        data_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data'),
-    )
-    install_production_guards(app)
 
     app.secret_key = secret
     port = int(os.environ.get('PORT', 5000))
